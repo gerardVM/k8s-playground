d7c75ffc380c7d181fcf85ce5dec274e3c76407f
GerardVM <101387700+gerardVM@users.noreply.github.com>
2025-08-03 17:09:40 +0200

    refactor(terraform): Set helm releases as list instead of objects

--- terraform/k8s/10-locals.tf
+++ terraform/k8s/10-locals.tf
@@ -1,4 +1,4 @@
 locals {
   config        = yamldecode(file("${path.module}/config.yaml"))
-  helm_releases = { for name, values in local.config.helm_releases : name => values if !try(values.disabled, false) }
+  helm_releases = { for helm_release in local.config.helm_releases : helm_release.name => helm_release if !try(helm_release.disabled, false) }
 }

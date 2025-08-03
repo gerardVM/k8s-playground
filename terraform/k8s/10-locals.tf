1ec745c03d88386108f5603a2941c017fc04ee0e
GerardVM <101387700+gerardVM@users.noreply.github.com>
2025-08-03 23:42:23 +0200

    refactor(terraform): Separate system from applications

--- terraform/k8s/10-locals.tf
+++ terraform/k8s/10-locals.tf
@@ -1,6 +1,7 @@
 locals {
-  config        = yamldecode(file("${path.module}/config.yaml"))
-  helm_releases = { for helm_release in local.config.helm_releases : 
+  system_config = yamldecode(file("${path.module}/config/system.yaml"))
+  apps_config   = yamldecode(file("${path.module}/config/applications.yaml"))
+  helm_releases = { for helm_release in concat(local.system_config.helm_releases, local.apps_config.helm_releases) :
                     "${helm_release.namespace.name}.${helm_release.name}" => helm_release
                       if !try(helm_release.disabled, false)
                   }

e30a55cd4c7d3ae3b484cc77c976da8a042e749c
GerardVM <101387700+gerardVM@users.noreply.github.com>
2025-08-03 23:19:03 +0200

    feat(my-rest-api): Include different environments

--- terraform/k8s/10-locals.tf
+++ terraform/k8s/10-locals.tf
@@ -1,4 +1,7 @@
 locals {
   config        = yamldecode(file("${path.module}/config.yaml"))
-  helm_releases = { for helm_release in local.config.helm_releases : helm_release.name => helm_release if !try(helm_release.disabled, false) }
-}
+  helm_releases = { for helm_release in local.config.helm_releases : 
+                    "${helm_release.namespace.name}.${helm_release.name}" => helm_release
+                      if !try(helm_release.disabled, false)
+                  }
+} 

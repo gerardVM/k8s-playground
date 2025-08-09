934eb75369f77a759e7a2521dc3701f11bdb9d3e
GerardVM <101387700+gerardVM@users.noreply.github.com>
2025-08-09 21:08:09 +0200

    fix(sops): Avoid creation order issues

--- terraform/k8s/10-locals.tf
+++ terraform/k8s/10-locals.tf
@@ -2,11 +2,12 @@ locals {
   system_config = yamldecode(file("${path.module}/config/system.yaml"))
   apps_config   = yamldecode(file("${path.module}/config/applications.yaml"))
 
+  namespaces    = toset(distinct([ for release in local.helm_releases : release.namespace ]))
+
   secrets_path  = "${path.module}/../../k8s/secrets"
   k8s_secrets   = { for f in fileset(local.secrets_path, "*.yaml") : f => "${local.secrets_path}/${f}" }
   k8s_secrets_d = { for k,v in data.sops_file.secret : k => yamldecode(v.raw) }
 
-  namespaces    = toset(concat(local.system_config.namespaces, local.apps_config.namespaces))
   helm_releases = { for helm_release in concat(local.system_config.helm_releases, local.apps_config.helm_releases) :
                     "${helm_release.namespace}.${helm_release.name}" => helm_release
                       if !try(helm_release.disabled, false)

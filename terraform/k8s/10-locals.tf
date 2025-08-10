2545ce4363165a8b5c22600584b1965f83d9c775
GerardVM <101387700+gerardVM@users.noreply.github.com>
2025-08-10 20:50:22 +0200

    refactor: Simlify the repo

--- terraform/k8s/10-locals.tf
+++ terraform/k8s/10-locals.tf
@@ -4,9 +4,13 @@ locals {
     "${helm_release.namespace}.${helm_release.name}" => helm_release if !try(helm_release.disabled, false)
   }
 
-  values_path   = "${local.k8s_path}/values"
-  k8s_path      = "${path.module}/../../k8s/terraform"
-  k8s_manifests = { for f in fileset("${local.k8s_path}/manifests", "*.yaml") : f => "${local.k8s_path}/manifests/${f}" }
-  k8s_secrets   = { for f in fileset("${local.k8s_path}/secrets", "*.yaml") : f => "${local.k8s_path}/secrets/${f}" }
+  k8s_path  = "${path.module}/../../k8s/terraform"
+  k8s_files = {
+    for dir in ["values", "manifests", "secrets"] :
+      dir => {
+        for f in fileset("${local.k8s_path}/${dir}", "*") :
+          replace(f, ".yaml", "") => "${local.k8s_path}/${dir}/${f}"
+      }
+  }
   k8s_secrets_d = { for k, v in data.sops_file.secret : k => yamldecode(v.raw) }
 }
